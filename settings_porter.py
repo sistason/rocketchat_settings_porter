29f1ef72eebd8642770a088384bfb721bd5f65e7
Kai <sistason@sistason.de>
2021-05-22 22:34:01 +0200

    added logging and removed prints

--- settings_porter.py
+++ settings_porter.py
@@ -1,9 +1,13 @@
 #!/bin/python3
 from rocketchat_API.rocketchat import RocketChat
 from requests.sessions import Session
+import os
 import json
 import time
-import os
+import logging
+
+
+logger = logging.getLogger(__name__)
 
 
 class Porter:
@@ -22,6 +26,7 @@ class Porter:
     def _wait_for_rocketchat(self, rocket=None):
         self.rocket = rocket
         timeout = 60
+        logger.info("starting to wait for rocketchat...")
         while timeout:
             time.sleep(1)
             try:
@@ -29,18 +34,23 @@ class Porter:
                     self.rocket = RocketChat(self.username, self.password, server_url=self.host, session=self.session)
                 ret = rocket.info().json()
                 if ret.get('success'):
+                    logger.info("rocketchat available!")
                     return
             except ConnectionError:
                 pass
             except:
                 pass
             timeout =- 1
+        logger.error("Timeout while waiting for rocketchat!")
+        
 
     def import_settings(self):
         with open(self.destination, 'r') as f:
             settings_to_import = json.load(f)
 
+        logger.info(f"Importing {len(settings_to_import)} settings...")
         self._import(settings_to_import)
+        logger.info("Finished importing settings!")
 
     def _import(self, settings):
         import_oauth = {}
@@ -54,7 +64,7 @@ class Porter:
         if import_oauth:
             # create all neccessary OAuth services
             for i, oauth_service_name in enumerate(import_oauth.keys()):
-                print(f'create {oauth_service_name}')
+                logger.debug(f'create {oauth_service_name}')
                 json_ = json.dumps({'msg': 'method', 'method': 'addOAuthService', 'params': [oauth_service_name], 'id': 1337+i})
                 self.rocket.call_api_post("method.call/addOAuthService", message=json_)
 
@@ -67,7 +77,7 @@ class Porter:
         data = self.rocket.settings().json()
         settings = {sett_.get('_id'): sett_.get('value') for sett_ in data.get('settings', [])}
         if not settings:
-            print('Nothing returned from server, just "{}"!'.format(data))
+            logger.error('Nothing returned from server, just "{}"!'.format(data))
             return {}
 
         api_default_count = settings.get('API_Default_Count', 50)
@@ -81,16 +91,17 @@ class Porter:
         return settings
 
     def export_changed(self):
+        logger.info("Exporting all non-default settings...")
         version = self.rocket.info().json().get('info', {}).get('version', 0)
         if not version:
-            print('Could not get server version!')
+            logger.error('Could not get server version!')
             return
 
         try:
             with open(os.path.join(self.DEFAULTS_PATH, '{}.json'.format(version)), 'r') as f:
                 settings_defaults = json.load(f)
         except FileNotFoundError:
-            print('No defaults for version {} found in the directory {}!'.format(version, self.DEFAULTS_PATH))
+            logger.error('No defaults for version {} found in the directory {}!'.format(version, self.DEFAULTS_PATH))
             return
 
         settings = self._export()
@@ -102,17 +113,22 @@ class Porter:
 
         with open(self.destination, 'w') as f:
             json.dump(settings_updated, f)
+        logger.info(f"Finished exporting {len(settings_updated)} updated settings")
 
     def export_all(self):
+        logger.info("Exporting all settings...")
         settings = self._export()
         with open(self.destination, 'w') as f:
             json.dump(settings, f)
+        logger.info(f"Finished exporting {len(settings)} settings")
 
     def close(self):
         self.session.close()
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
     import sys
     if sys.argv[1] == 'test':
         from tests.settings_porter_tests import test
